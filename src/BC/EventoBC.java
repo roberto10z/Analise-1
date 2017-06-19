d20f0d745386f93dd623659a2ddbe7dd7c812922
ROBERTO <ROBERTO@ROBERTO-PC>
2017-06-18 23:23:12 -0300

    codigo

---
+++ src/BC/EventoBC.java
@@ -0,0 +1,38 @@
+package BC;
+
+public class EventoBC {
+
+	
+	private long id;
+		private String nome;
+		private String nome, descricao;
+	 	private Date dataEvento;
+		private String descricao;
+		private TipoEventoEnum tipoEventoEnum;
+		private Ambiente ambiente;
+		private List<Artista> artistas;
+		private Diretor diretor;
+		
+		
+		public void setIngressos(List<Ingresso> ingressos) {
+			this.ingressos = ingressos;
+		}
+	 
+	 	
+					this.ingressos = new ArrayList<Ingresso>();
+					criaIngressos();
+			
+			 	}
+			 
+			
+				this.ingressos = new ArrayList<Ingresso>();
+					criaIngressos();
+				}
+			
+				this.diretor = diretor;
+					this.ingressos = new ArrayList<Ingresso>();
+					criaIngressos();
+			 	}
+	
+	
+}
